0e5e1a4266af24c5f4a3897047f590c8a61bd6d7
sadiaism <ismailmoiz80@gmail.com>
2025-02-07 15:31:50 +0500

    the DashBoard

---
+++ app/protected/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const isLoggedIn = localStorage.getItem("isLoggedIn");
+    if (!isLoggedIn) {
+      router.push("/admin");
+    }
+  }, [router]);
+
+  return <>{children}</>;
+};
+
+export default ProtectedRoute;
