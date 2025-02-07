6599da210bf25cf3228b531087d7e4dccd837869
sadiaism <ismailmoiz80@gmail.com>
2025-02-07 15:15:36 +0500

    the dash board

--- app/components/protected/page.tsx
+++ app/components/protected/page.tsx
@@ -24,14 +24,10 @@
 
 
 "use client";
-import React, { ReactNode, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
-type ProtectedRouteProps = {
-  children: ReactNode;
-};
-
-const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
+const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
 
   useEffect(() => {
